997106594aa2be9e7ea86aa11a9b4a988758f1cb
Aaron Ewall-Wice <aaronew@mit.edu>
2017-08-03 17:42:37 -0400

    testing farField export

---
+++ src/test_export_farField.py
@@ -0,0 +1,13 @@
+from farfieldData import FarFieldData
+import numpy as np
+
+beamDir='../data/beams/varyRiggingHeight_short/beamVaryRiggingHeight_run000002/'
+beamDirFeedOnly='../data/beams/beamCosmicTwilightPolarimeter_diskConnect_short/'
+beamFile=beamDir+'farfield (f=%s) [1].txt'%('100')
+beamFileFeedOnly=beamDirFeedOnly+'farfield (f=%s) [1].txt'%('100')
+rigging_height=4.40
+farfield=FarFieldData([beamFileFeedOnly],[beamFile],
+                      ['100'],64,pols=['X'],rotateY=True,
+                      invertFeedOnly=False,
+                      rotatexz=True,
+                      dFocus=rigging_height)
