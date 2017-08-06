5f0043acc3fa1c1bbaa76523a53da8116a4652bf
Aaron Ewall-Wice <aaronew@mit.edu>
2017-08-06 16:23:25 -0400

    script to run prisim simulations.

---
+++ src/run_prisim_beam_analysis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ~/Python/analysis/bin/activate
+#models=( tall short )
+#heights=( 4 9 )
+#zs=( 13 14 15 16 )
+models=( tall )
+heights=( 4 )
+zs=( 13 )
+export TMPDIR=/tmp/
+for model in "${models[@]}"; do
+    for h in "${heights[@]}"; do
+	for z in "${zs[@]}"; do 
+	    mpirun -n 2 python ~/Python/analysis/bin/run_prisim.py -i /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_ctp_${model}_z${z}_h4p${h}m.yaml
+	done
+    done
+done
+
+
--- src/test_export_farField.py
+++ src/test_export_farField.py
@@ -1,11 +1,11 @@
 from farfieldData import FarFieldData
 import numpy as np
 
-beamDir='../data/beams/varyRiggingHeight_short/beamVaryRiggingHeight_run000002/'
+beamDir='../data/beams/varyRiggingHeight_short/beamVaryRiggingHeight_run000003/'
 beamDirFeedOnly='../data/beams/beamCosmicTwilightPolarimeter_diskConnect_short/'
 beamFile=beamDir+'farfield (f=%s) [1].txt'%('100')
 beamFileFeedOnly=beamDirFeedOnly+'farfield (f=%s) [1].txt'%('100')
-rigging_height=4.40
+rigging_height=4.90
 farfield=FarFieldData([beamFileFeedOnly],[beamFile],
                       ['100'],64,pols=['X'],rotateY=True,
                       invertFeedOnly=False,
