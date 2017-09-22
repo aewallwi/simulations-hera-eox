724cf3e113b619141ecbeb63a61a6af7123673a1
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-09-22 09:58:50 -0700

    added beam lists for sinuous and modified convert_beams_prisim.sh to process them.

--- src/convert_beams_prisim.sh
+++ src/convert_beams_prisim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source ~/Python/analysis/bin/activate
 
-python cst2prisim_fits.py -b beamlist_short.txt -f freq_list.txt -o /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_tall/ 
+#python cst2prisim_fits.py -b beamlist_short.txt -f freq_list.txt -o /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_tall/ 
 
-python cst2prisim_fits.py -b beamlist_tall.txt -f freq_list.txt -o /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_short/
+#python cst2prisim_fits.py -b beamlist_tall.txt -f freq_list.txt -o /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_short/
+
+python cst2prisim_fits.py -b beamlist_sinuous.txt -f freq_list_sinuous.txt -o /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/Sinuous_Beams/
