ec982e526cc91029cdf1e5bd4564bf566fd0779f
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-09-27 10:02:06 -0700

    conf files for sinuous analysis

--- src/run_prisim_sinuous_beam_analysis.sh
+++ src/run_prisim_sinuous_beam_analysis.sh
@@ -2,18 +2,17 @@
 #source ~/Python/analysis/bin/activate
 source activate prisim
 growthrates=( 0.5 0.8 )
-bheigts=( 50 90 )
+bheights=( 50 90 )
 zs=( 13 14 15 16 )
-#models=( tall )
-#heights=( 4 )
-#zs=( 13 )
 export TMPDIR=/tmp/
 for gr in "${growthrates[@]}"; do
+    echo $gr
     for bh in "${bheights[@]}"; do
-	if [ $bh != "50" ] || [ ! $gr != "0.8" ]
+	echo $bh
+	if [ $bh != "50" ] #|| [  $gr != "0.8" ]
 	then
 	    for z in "${zs[@]}"; do 
-		mpirun -n 2 python ~/Python/analysis/bin/run_prisim.py -i /Users/aaronew/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_sinuous_gr_${gr}_bh_${bh}_z${z}.yaml
+		mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_sinuous_gr_${gr}_bh_${bh}_z${z}.yaml
 	    done
 	fi
     done
