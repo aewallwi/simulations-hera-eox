4f486c016a7c11be623333a825f3db6de704c1cc
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-08 16:42:00 -0700

    convertible beam prisim scripts added.

---
+++ src/run_prisim_convertible_beam_analysis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#run prisim simulations for convertible feed
+source activate prisim
+zs=( 06 07 08 09 10 11 12 13 14 15 16 )
+export TMPDIR=/tmp/
+for z in "${zs[@]}"; do 
+    mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_convertible_z${z}.yaml
+done
