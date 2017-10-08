e36091a115e74962340027186080d1ee6b0b6bc6
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-08 15:24:44 -0700

    fixed inversion parameters for ocnvertible beam beam conversion.

--- src/convert_beams_convertible.sh
+++ src/convert_beams_convertible.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #source ~/Python/analysis/bin/activate
-#source activate hera_antenna
-#python cst2prisim_fits.py -b beamlist_short.txt -f freq_list.txt -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_tall/ 
-
-#python cst2prisim_fits.py -b beamlist_tall.txt -f freq_list.txt -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_short/
-
-python cst2prisim_fits.py -b beamlist_convertible.txt -f freq_list_convertible.txt -i False -r False -q True -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/Sinuous_Beams
+source activate hera_antenna
+#convert beams for convertible HERA antenna.
+python cst2prisim_fits.py -b beamlist_convertible_low.txt -f freq_list_convertible_low.txt -i True -r False -q False -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/Convertible_Beams
+python cst2prisim_fits.py -b beamlist_convertible_high.txt -f freq_list_convertible_high.txt -i True -r False True -q False -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/Convertible_Beams
