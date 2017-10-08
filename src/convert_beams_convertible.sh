c95397334149228460f1e39187610ec478301c73
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-08 15:04:18 -0700

    added convertible feed conversions scripts and config.

---
+++ src/convert_beams_convertible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#source ~/Python/analysis/bin/activate
+#source activate hera_antenna
+#python cst2prisim_fits.py -b beamlist_short.txt -f freq_list.txt -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_tall/ 
+
+#python cst2prisim_fits.py -b beamlist_tall.txt -f freq_list.txt -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/CTP_beams_short/
+
+python cst2prisim_fits.py -b beamlist_convertible.txt -f freq_list_convertible.txt -i False -r False -q True -o /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/Sinuous_Beams
