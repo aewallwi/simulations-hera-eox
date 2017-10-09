971fa59638090e658688a6b5b7f9a06550514ccc
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-08 17:21:26 -0700

    fixed bug in cst2prisim_fits.py that involved mixing frequency order of beams.

--- src/cst2prisim_fits.py
+++ src/cst2prisim_fits.py
@@ -2,19 +2,61 @@ import numpy as np
 from farfieldData import Beam
 from optparse import OptionParser
 import re
+import glob
 usage=('Converts a list of CST beam files into a list of healpix .fits files'
        'that are compatible with the prisim.')
 parser=OptionParser(usage=usage)
 parser.add_option('-b','--beams',dest='beamdirlist',help="text list of CST beam files")
 parser.add_option('-f','--freqs',dest='freqlist',help="text list of frequencies in MHz")
 parser.add_option('-o','--output',dest='outputdir',help="output directory")
+parser.add_option('-r','--rotatexz',dest='rotatexz',help="rotate xz, default=True",default="True")
+parser.add_option('-i','--invert',dest='invert',help="invert beam,default=True",default="True")
+parser.add_option('-q','--rotatexy',dest='rotatexy',help="rotate xy, default=True",default="False")
 (options,args)=parser.parse_args()
 beamdirs=open(options.beamdirlist).readlines()
 fstrlist=open(options.freqlist).readlines()
+
+assert options.rotatexz in ["True","False"]
+assert options.invert in ["True","False"]
+
+if options.rotatexz=="True":
+    rotatexz=True
+else:
+    rotatexz=False
+if options.invert=="True":
+    invert=True
+else:
+    invert=False
+if options.rotatexy=="True":
+    rotatexy=True
+else:
+    rotatexy=False
+print('invert=%s'%invert)
+print('rotatexz=%s'%rotatexz)
+    
 for beamdir in beamdirs:
-    beamname=beamdir.split('/')[-1][:-1]
-    beam=Beam(pols=['X'],rotateY=True,rotatexz=True,invert=True)
-    flist=[re.sub('\n','',(beamdir+'/farfield (f=%s) [1].txt')%(s)) for s in fstrlist]
+    beamdir=re.sub('\n','',beamdir)
+    flist_unorg=glob.glob(beamdir+'/*')
+    #print beamdir
+    #print flist_unorg
+    beamdir=beamdir.split('/')
+    if len(beamdir[-1])<2:
+        beamname=beamdir[-2]
+    else:
+        beamname=beamdir[-1]
+    #print('beamname=%s'%beamname)
+    beam=Beam(pols=['X'],rotateY=True,rotatexz=rotatexz,invert=invert,rotatexy=rotatexy)
+    #flist=[re.sub('\n','',(beamdir+'/farfield (f=%s) [1].txt')%(s)) for s in fstrlist]
+    flist=[]
+    #reorder flist according to frequencies
+    for f in fstrlist:
+        for filename in flist_unorg:
+            if str(f)[:-1] in filename and not( float(f[:-1])<100 and ('1'+str(f)[:-1] in filename or '2'+str(f)[:-1] in filename)):
+                flist.append(filename)
+
+    #print fstrlist
+    #print flist
+    
     beam.read_files(fstrlist,flist)
     freqlist=np.array([float(f) for f in fstrlist])*1e6
     beam.export_fits_prisim(options.outputdir+beamname+'.fits',['I'],
