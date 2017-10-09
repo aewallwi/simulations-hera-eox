78b241f39e6554c234830b2a83c95e6284db6a69
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-08 22:25:13 -0700

    added high band simulations.

---
+++ src/run_prisim_highband_analysis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#run prisim simulations for vivaldi feed
+source activate prisim
+zs=( 06 07 08 09 10 11 12 )
+export TMPDIR=/tmp/
+for z in "${zs[@]}"; do 
+    mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_highband_z${z}.yaml
+done
