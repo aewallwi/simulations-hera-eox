f9447be3cebbce54d6c1acd89731d22f72d64e46
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-11 12:01:56 -0700

    fixed bp=50cm to bp=90cm

--- src/run_prisim_lst0_analysis.sh
+++ src/run_prisim_lst0_analysis.sh
@@ -3,6 +3,6 @@
 source activate prisim
 export TMPDIR=/tmp/
 
-mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_vivaldi_z09_lst0.yaml
+mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_vivaldi_z08_lst0.yaml
 mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_sinuous_gr_0.5_bh_50_z08_lst0.yaml
-mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_highband_z09_lst0.yaml
+mpirun -n 2 python /Users/ewallwic/miniconda2/envs/prisim/bin/run_prisim.py -i /Users/ewallwic/Dropbox_MIT/Science/simulations-hera-eox/data/simulations/hera_19_highband_z08_lst0.yaml
