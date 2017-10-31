10570f48e23b8aeb2312041b7005d5af67f43664
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2017-10-31 12:07:05 -0700

    added hirax adaptive mesh interpolation.

---
+++ src/interpolate_traces_HIRAX_adaptive_mesh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source activate hera_antenna
+
+ttdir=/Users/ewallwic/Dropbox_MIT/Science/HIRAX/HIRAX_adaptive_mesh_results/
+
+chdir=${ttdir}HERA_Convertible_high_resend/
+cldir=${ttdir}HERA_Convertible_low_resend/
+
+
+vdir=${ttdir}Vivaldi/
+ifile=hirax_adaptive_mesh_i1.txt
+fbase=hirax_adaptive_mesh
+labels=( pass1 pass2 pass3 pass4 pass5 pass6 )
+for label in "${labels[@]}";
+do
+    python standardize_time_series.py -i ${ttdir} -o ${ttdir}${fbase}_${label}_o1.txt -n ${ttdir}${fbase}_interp.txt
+done
+
+
+
+
+
